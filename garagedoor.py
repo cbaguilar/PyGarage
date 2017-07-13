ad62690f05a69fd5ecb139ee49419c18e4e0b21c
Mr. Frog <christianajusd@gmail.com>
2017-07-13 16:50:19 -0700

    Added client and server apps

---
+++ garagedoor.py
@@ -0,0 +1,58 @@
+# -*0 coding: utf-8 -*-
+
+
+import sys
+import socket
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
+
+HOST, PORT = "localhost", 31415 
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+class Example(QWidget):
+
+    def lift(self):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((HOST,PORT))
+            sock.sendall("click".encode())
+            recieved = sock.recv(1024).decode()
+            print(recieved)
+        except Exception as e:
+            print(e)
+        finally:
+            print("closing socket")
+            sock.close()
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):
+        
+        qbtn = QPushButton('Click',self)
+        qbtn.clicked.connect(self.lift)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50,50)
+         
+        '''
+        qbtn = QPushButton('Quit',self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50,50)
+        '''  
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))        
+    
+        self.show()
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())  
