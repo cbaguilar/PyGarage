4c9c0b65aa38d3b88796ced598e58a52b5d53625
Mr. Frog <christianajusd@gmail.com>
2017-07-13 17:14:35 -0700

    Added gpio lifting mechanism

--- gDoorServer.py
+++ gDoorServer.py
@@ -1,4 +1,27 @@
 import socketserver
+import time
+
+LIB = False
+
+try:
+    import RPi.GPIO as gp
+    LIB = True
+    print("Loaded GPIO library")
+    gp.setmode(gp.BOARD)
+    gp.setup(16,gp.OUT)
+    gp.output(16,1)
+except Exception:
+    LIB = False
+    print("Could not load GPIO library")
+
+def lift():
+    print("Lifting...")
+    if LIB:
+        gp.output(16,0)
+        time.sleep(0.5)
+        gp.output(16,1)
+    else:
+        print("No GPIO, simulating lift...")
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
@@ -14,6 +37,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
         print(self.data.decode())
+        lift()
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
 
@@ -26,3 +50,4 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+    print("Starting server")
