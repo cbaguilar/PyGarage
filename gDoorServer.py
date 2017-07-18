a947477991529033289b5a40b684c69911cbe446
Christian A <christianajusd@gmail.com>
2017-07-17 22:20:17 -0700

    moved gpio to free pins

--- gDoorServer.py
+++ gDoorServer.py
@@ -11,28 +11,48 @@ try:
     import RPi.GPIO as gp
     LIB = True
     print("Loaded GPIO library")
+    gp.cleanup()
     gp.setmode(gp.BOARD)
     gp.setup(16,gp.OUT)
     gp.output(16,1)
     
-    gp.setup(18,gp.IN, pull_up_down=gp.PUD_UP)
-    gp.setup(22,gp.IN, pull_up_down=gp.PUD_UP)
+    gp.setup(8,gp.IN, pull_up_down=gp.PUD_UP)
+    gp.setup(10,gp.IN, pull_up_down=gp.PUD_UP)
+    gp.setup(12,gp.OUT)
 except Exception:
     LIB = False
     print("Could not load GPIO library")
+
+def doorSens():
+    return gp.input(8)
+def lockSens():
+    return gp.input(10)
+def lockLight(val):
+    gp.output(12,val)
+def updateLight():
+    if LIB:
+        while True:
+            if lockSens():
+                lockLight(False)
+            else:
+                lockLight(True)
+        time.sleep(1)
+
+
 
 def checkDoor():
     while True:
         #print("Starting door check...")
         time.sleep(1)
         if LIB:
-            if gp.input(22):
-                global LOCKED; LOCKED = True
+            if lockSens():
+                #lockLight(False)
+                global LOCKED; LOCKED = False
                 print("Checking door...")
-                if gp.input(18):
+                if doorSens():
                     print("Door open, waiting 60 seconds for close")
                     time.sleep(60)
-                    if gp.input(18):
+                    if doorSens() and lockSens():
                         print("Door still open, activating lift")
                         lift()
                     else:
@@ -44,6 +64,7 @@ def checkDoor():
                 if LOCKED == False:
                     print("Door Locked")
                 global LOCKED; LOCKED = True
+                #lockLight(True)
         else:
                 print("Simulating door check...")
                 time.sleep(20)
@@ -85,6 +106,7 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     _thread.start_new_thread(checkDoor,())
+    _thread.start_new_thread(updateLight,())
     server.serve_forever()
     print("Starting server")
     for x in range (0,9):
